e8662aa50bbcee2f57a5b7131e98a950ba194168
Joy Nag <joyruet06@gmail.com>
2012-03-21 14:02:12 +0600

    Python practice update

--- python/ex3.py
+++ python/ex3.py
@@ -4,4 +4,12 @@ print "Hens",20 + 30 /6
 
 print "Rosters ",100-23*3%4
 
-print 
+print "This one is Created By me"
+
+print 3 + 2 +1 - 5 +4 %2 -1 / 4 +6
+
+print "Is is true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "How about some more?"
---
+++ python/ex4.py
@@ -0,0 +1,18 @@
+my_name = "Joy K. Nag"
+my_age = 24	#not a lie
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+
+
+print "Lets talk about %s." %my_name
+print "He's %d inches tall." %my_height
+print "He's %d punds heavy." %my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." %(my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+
